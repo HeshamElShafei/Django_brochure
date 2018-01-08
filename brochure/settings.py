53c64438ef41591d24cba7c30a12a824eabe01ea
Hesham ElShafei <he674212@ohio.edu>
2018-01-08 13:25:50 +0200

    10th commit

--- brochure/settings.py
+++ brochure/settings.py
@@ -18,11 +18,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/clubs/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'clubs/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
@@ -127,4 +127,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/clubs/static/'
+STATIC_URL = '/static/'
